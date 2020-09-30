aee8dd697464d3eb3129a2114a2d4a4be715d199
reini <reinhold.schachner@hotmail.com>
2020-09-30 16:33:32 +0200

    cleared board and moved domcaching out of render-function

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //gameboard
 const gameBoard = ( () => {
-    let board = ["X","O","X","O","X","O","X","O","X"];
+    let board = ["","","","","","","","",""];
+    const displayElements = document.querySelectorAll(".gb-field");
 
     function render() {
-        const displayElements = document.querySelectorAll(".gb-field");
-        
         for (let i = 0; i < board.length; i++) {
             displayElements[i].textContent = board[i];
-        }
-        
+        }  
     }
 
     return {render};
